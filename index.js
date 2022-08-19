6bd6abda3854369cac415ddbda6f6d48103bc02c
farhiya dahir <farhiya.dahir@student.moringaschool.com>
2022-08-19 11:22:41 +0300

    Completed assgnment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate'){
+    return `This Saturday, I want to ${defaultActivity}!`
+}
+function mondayWork(defaultActivity ='go to the office'){
+    return `This Monday, I will ${defaultActivity}.`
+}
+
+function wrapAdjective(wrapper = '*'){
+    return function (emphatic = 'special'){
+        return `You are ${wrapper}${emphatic}${wrapper}!`
+    }
+}
